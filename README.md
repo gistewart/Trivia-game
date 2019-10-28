fc012980dbbacdce9eacbd2ec54c43aec4ec5374
Graeme Stewart <gi_stewart@yahoo.com>
2019-10-28 19:49:30 -0400

    added code for timer

---
+++ README.md
@@ -0,0 +1,19 @@
+# CrystalsCollector Game
+
+## Project Functionality
+This is a game for web browsers where the user must match a target number by adding together up to 4 other numbers that are only revealed when images of crystals are clicked.
+
+## Project Usefulness
+This project demonstrates the applicaton of the jQuery library including content updates to the HTML file based on user behavior.
+
+## How to get started
+The game will be ready to play when the page loads. After each round is played, either the Wins or Losses value will change and the game will be ready to play again. A refresh of the page resets the Wins and Losses to zero. Other instructions are detailed on the web site.
+
+## How to get help
+[jQuery Official Website](https://jquery.com/)
+
+## Project maintenance and contributions
+This is not an original game.  Instead, this project was prepared as part of the work for Georgia Tech's Coding Boot Camp.
+
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,43 @@ $(document).ready(function() {
     //declare global variables
     userAnsArr = [];
 
+    function hideQuestions() {
+        $(".questions").hide();
+    }
+
+    hideQuestions();
+
+    // Timer
+    var number = 10;
+    var intervalId;
+
+    function runTimer() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+        number--;
+        minutes = parseInt(number / 60, 10);
+        seconds = parseInt(number % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        $("#timer").html("<h2>" + "Time left: " + minutes + ":" + seconds + "</h2>");
+        if (number === 0) {
+            stop();
+            hideQuestions();
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+    $(".btn").on("click", function() {
+        $(".questions").show();
+        runTimer();
+    });
+
     $(".Q01").on("click", function() {
         console.log("Q1 just got clicked");
         let answer = $('input[name="optradio01"]:checked').val();
@@ -26,5 +63,4 @@ $(document).ready(function() {
         userAnsArr[2] = answer;
         console.log(userAnsArr);
     });
-
 });
