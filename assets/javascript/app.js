8f0283b3ad1bf84071f1ceaf11e354e4f78c2254
Graeme Stewart <gi_stewart@yahoo.com>
2019-10-29 21:42:47 -0400

    added functioning code for all sections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
     //declare global variables
+    let numUserCorrectAns = 0;
+    let numUserIncorrectAns = 0;
+    let numUserEmpties = 0;
     let userAnsArr = [];
     const correctAnsArr = ["01C", "02D", "03A"];
 
@@ -23,6 +26,12 @@ $(document).ready(function() {
     hideQuestions();
     hideResults();
 
+    $(".start").on("click", function() {
+        $(".opening").hide();
+        $(".questions").show();
+        runTimer();
+    });
+
     // Timer
     var number = 10;
     var intervalId;
@@ -43,6 +52,7 @@ $(document).ready(function() {
             stop();
             hideQuestions();
             showResults();
+            calcResults();
         }
     }
 
@@ -50,13 +60,12 @@ $(document).ready(function() {
         clearInterval(intervalId);
     }
 
-    $(".btn").on("click", function() {
-        $(".opening").hide();
-        $(".questions").show();
-        // runTimer();
+    $(".done").on("click", function() {
+        stop();
+        hideQuestions();
+        showResults();
+        calcResults();
     });
-
-
 
     $(".Q01").on("click", function() {
         console.log("Q1 just got clicked");
@@ -80,19 +89,49 @@ $(document).ready(function() {
         console.log(answer);
         userAnsArr[2] = answer;
         console.log(userAnsArr);
-        console.log(correctAnsArr);
+    });
+    //Function to calculate the results
 
-        var results1 = [];
-        for (var i = 0; i < userAnsArr.length; i++) {
+
+    function calcResults() {
+        //console log correct answers and user's answers
+        console.log(correctAnsArr);
+        console.log(userAnsArr);
+
+        //set up array to store user's correct answers only
+        let userCorrectAnsArr = [];
+
+        //push correct answers into this array by looping through user's answers and check to see if they are included in the correct answer array
+        for (let i = 0; i < userAnsArr.length; i++) {
             if (correctAnsArr.indexOf(userAnsArr[i]) !== -1) {
-                results1.push(userAnsArr[i]);
+                userCorrectAnsArr.push(userAnsArr[i]);
             }
         }
-        console.log(results1);
-        console.log(correctAnsArr.length);
+        console.log(userCorrectAnsArr);
 
-        var empties = userAnsArr.length - userAnsArr.filter(function(x) { return true }).length;
-        console.log(empties);
-    });
+        //determine NUMBER of correct answers
+        numUserCorrectAns = userCorrectAnsArr.length;
+        console.log("Number of correct answers: " + numUserCorrectAns);
 
+        // check for NUMBER of unanswered questions, allowing for all questions either being answered or all questions not being answered
+        console.log(userAnsArr.length);
+        let numUserEmpties = 0;
+        if (userAnsArr.length > 0) {
+            numUserEmpties = userAnsArr.length - userAnsArr.filter(function(x) { return true }).length;
+        } else if (userAnsArr.length == correctAnsArr.length) {
+            numUserEmpties = 0
+        } else {
+            numUserEmpties = correctAnsArr.length;
+        }
+        console.log("Number of unanswered questions: " + numUserEmpties);
+
+        //calculate NUMBER of incorrect answers
+        let numUserIncorrectAns = correctAnsArr.length - numUserCorrectAns - numUserEmpties;
+        console.log("Number of incorrect answers: " + numUserIncorrectAns);
+
+        //Write values to the DOM
+        $("#numUserCorrectAns").text("Correct answers: " + numUserCorrectAns);
+        $("#numUserIncorrectAns").text("Incorrect answers: " + numUserIncorrectAns);
+        $("#numUserEmpties").text("Unanswered questions: " + numUserEmpties);
+    }
 });
